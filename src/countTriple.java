2eac38fc482990cbcd7135c0d8fc6bc6a6919ffe
maathier al-nabhani <maathier.ahmed12@gmail.com>
2026-04-21 16:53:07 +0400

    TasK5sumDigits

--- src/countTriple.java
+++ src/countTriple.java
@@ -1,4 +1,16 @@
-package PACKAGE_NAME;
-
 public class countTriple {
+    public static Boolean equalIsNot(String str , String C) {
+
+
+        int len = str.length();
+        int count = 0;
+
+        for (int i = 0; i < len - 2; i++) {
+            char c = str.charAt(i);
+            if (c == str.charAt(i + 1) && c == str.charAt(i + 2)) {
+                count++;
+            }
+            return count;
+        }
+    }
 }
--- src/sumDigits.java
+++ src/sumDigits.java
@@ -1,4 +1,17 @@
-package PACKAGE_NAME;
-
 public class sumDigits {
-}
+    public static Integer equalIsNot(String str, String C) {
+        int len = str.length();
+        int sum = 0;
+
+        for (int i = 0; i < len; i++) {
+
+            if (Character.isDigit(str.charAt(i))) {
+
+                String C = str.substring(i,i+1);
+
+                sum += Integer.parseInt(C);
+            }
+        }
+        return sum;
+    }
+    }
