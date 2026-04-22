27ddc9e918057a6e7a94a309c356a46e3179e105
maathier al-nabhani <maathier.ahmed12@gmail.com>
2026-04-22 14:55:42 +0400

    Task11 notReplace

--- src/notReplace.java
+++ src/notReplace.java
@@ -1,5 +1,22 @@
 public class notReplace {
     public static String notReplace(String str) {
+        public String notReplace(String str) {
+            int len = str.length();
 
+            String result = "";
+
+            for(int i = 0; i < len; i++){
+                if(i-1 >= 0 && Character.isLetter(str.charAt(i-1))
+                        || i+2 < len && Character.isLetter(str.charAt(i+2))) {
+                    result += str.charAt(i);
+                }
+                else if(i+1 < len && str.substring(i, i+2).equals("is")) {
+                    result += "is not";
+                    i++;
+                }
+                else result += str.charAt(i);
+            }
+            return result;
+        }
     }
 }
