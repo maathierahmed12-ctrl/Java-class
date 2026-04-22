46d9d532f55b987eade3f1065e13b6c25e5ed960
maathier al-nabhani <maathier.ahmed12@gmail.com>
2026-04-22 14:23:07 +0400

    Task10 sumNumbers

---
+++ src/sumNumbers.java
@@ -0,0 +1,30 @@
+public class sumNumbers {
+
+    public static void main(String[] args) {
+
+        public static Integer sumNumbers(String str) {
+
+            int len = str.length();
+
+            int sum=0;
+            String temp ="";
+
+            for(int i =0; i<len;i++){
+                if(char.isDigit(str.charAt(i))){
+                    if(i<len-1&& Character.isDigit(str.chari+1))){
+
+                       temp += str.charAt(i);
+                    }
+                }else{
+
+                    if(! temp.equals("")){
+
+                    }
+                    sum += Integer.str(temp);
+                }
+            }
+                   return sum;
+        }
+
+}
+}
